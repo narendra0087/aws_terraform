bdd09057a4a0c7df389baec29bc77b5f71ca795c
narendra0087 <narendra087@gmail.com>
2019-06-10 18:57:10 +0530

    second commant

--- aws_ec2_ami.tf
+++ aws_ec2_ami.tf
@@ -1,4 +1,4 @@
-#ec2 Ubuntu machine AMI
+#ec2 Ubuntu machine
 
   data "aws_ami" "ubuntu" {
   most_recent = true
@@ -13,4 +13,9 @@
     values = ["hvm"]
   }
   owners = ["099720109477"]
+  }
+  #-------------key pair-----------------
+  resource "aws_key_pair" "terraform_ec2_key" {
+  key_name = "terraform_ec2_key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDd8TtHAqF7LJ/y9ZAhfQaqJC3fztpJDhdph9udEMPyZGlD1XK0qpxYyxgY17gAONAk10Ffgnfx7GQttB5UiTKl4jQqdX8WkCw+U2CSnTo8gfyDGQQcoZ5JXyUpTvzyPQgc+Fg+F+c+WDxghOjCoYOGlgB/71VswmUkUcNhaVmKlHwdwZpNWoTGN8RJ3cS0pvUP4+KTjXjsAFujwFAzdPxz8QJAIkD36i3g5X5e4XC/Ggcpr8TMyRF77pjd9K1mPTqD3z4zRw2t+P/I3MSpvr5Rw9zAj1HC7y8o2k0+AKo86D1VubWSX5uT5vmauxrgvPEig2IBctDPOkIfjwICe0Yn 100498@MP12AQMF"
   }
