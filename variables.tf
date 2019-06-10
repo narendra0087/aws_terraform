c48a0e98148f235df3a05f1d8d561fbcc557881b
narendra0087 <narendra087@gmail.com>
2019-06-10 15:31:35 +0530

    variable define

---
+++ variables.tf
@@ -0,0 +1,15 @@
+# AWS configuration
+
+variable "aws_access_key" {
+    default = "Your access key"
+}
+variable "aws_secret_key" {
+  default = "Aws access key"
+}
+variable "aws_region" {
+  default = "Aws region"
+}
+
+
+
+
