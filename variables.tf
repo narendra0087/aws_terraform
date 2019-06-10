6cd5e83726366f936b74b1edc9d4a369a79237fb
narendra0087 <narendra087@gmail.com>
2019-06-10 18:58:32 +0530

    second comment

--- variables.tf
+++ variables.tf
@@ -9,6 +9,25 @@ variable "aws_secret_key" {
 variable "aws_region" {
   default = "Aws region"
 }
+variable "availablity_zone" {
+  default = "us-east-1"
+}
+
+variable "vpc_cidr" {
+    description = "CIDR for the vpc"
+  default = "10.0.0.0/16"
+}
+variable "public_subnet_cidr" {
+  description = "Public subnet CIDR"
+  default = "10.0.1.0/24"
+}
+variable "private_subnet_cidr" {
+  description = "private subnet CIDR"
+  default = "10.0.2.0/24"
+}
+
+
+
 
 
 
