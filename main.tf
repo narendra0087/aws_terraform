a0bbf5e5a90783d46d2bf18c8f70f239778a63c5
narendra0087 <narendra087@gmail.com>
2019-06-10 22:04:23 +0530

    third commit

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "ec2" {
      
     key_name = "${aws_key_pair.terraform_ec2_key.id}"      
     subnet_id =  "${aws_subnet.pub_sub.id}"
-    vpc_security_group_ids = "${aws_security_group.sgweb.id}"
+    vpc_security_group_ids = ["${aws_security_group.sgweb.id}"]
     associate_public_ip_address = true
     source_dest_check = false
     user_data = "${file("install.sh")}" 
